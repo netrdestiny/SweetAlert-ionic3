2c622a5a8dc97385267735b01b672a1f0cd65288
Nayan <nayan.vekariya014@gmail.com>
2018-08-25 11:53:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# Plugin
+
+$ npm install sweetalert
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import swal from 'sweetalert';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {
 
-  }
+  }
+
+  SweetAlert(){
+    swal("Good job!", "You clicked the button!", "success");
+  }
 
 }
